d42062466c287fa384f9b0a2ff75987ea3bbca3e
GitHub Action <action@github.com>
2022-08-15 04:21:17 +0000

    Template cleanup

--- src/main/kotlin/com/github/fallending/ideatea/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/fallending/ideatea/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.fallending.ideatea.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.fallending.ideatea.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/fallending/ideatea/services/MyProjectService.kt
+++ src/main/kotlin/com/github/fallending/ideatea/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.fallending.ideatea.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.fallending.ideatea.MyBundle
 
 class MyProjectService(project: Project) {
 
